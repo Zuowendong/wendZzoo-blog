b42fa1d58e01d7c07823bf73fbaea34099845cfe
zwd <17326195691@163.com>
2024-01-08 14:30:17 +0800

    reactive和effect依赖收集触发依赖

--- .vitepress/vueSource.config.ts
+++ .vitepress/vueSource.config.ts
@@ -5,6 +5,10 @@ export default [
         text: "1.搭建项目环境，集成ts和jest",
         link: "/vueSource/",
       },
+      {
+        text: "2.reactive和effect依赖收集触发依赖",
+        link: "/vueSource/2",
+      },
     ],
   },
 ];
