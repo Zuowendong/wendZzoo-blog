6271de829d017631a684cbf20f491467033ff8e9
wendzzoo <19844544551@163.com>
2024-10-10 09:03:14 +0800

    feat: add socket

--- .vitepress/earthworm.config.ts
+++ .vitepress/earthworm.config.ts
@@ -5,6 +5,10 @@ export default [
         text: "如何搭建 Nuxt3+Nest 全栈式项目",
         link: "/earthworm/index",
       },
+      {
+        text: "websocket+redis 展示实时在线人数",
+        link: "/earthworm/websocket+redis展示实时在线人数",
+      },
     ],
   },
 ];
---
+++ earthworm/websocket+redis展示实时在线人数.md
@@ -0,0 +1,7 @@
+# websocket+redis 展示实时在线人数
+
+## 前言
+
+公司的屎山看多了，来学学 earthworm 中优雅代码。
+
+earthworm 是一款通过连词成句这种更新颖方式学习英语的一款软件，其源码仓库在 github 上 starred 已经有 3.1k。仓库地址在这儿，[点击查看](https://github.com/cuixueshe/earthworm)。
